10724b18f262f5b8d7a1331524928b45ca35801e
lenin123321 <leninquispemamani@gmail.com>
2024-01-18 04:07:12 +0100

    1

--- react/primer-ejercicio-react/src/App.js
+++ react/primer-ejercicio-react/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/Estilos.css";
+import { useState } from "react";
+import React from "react";
+export default function App() {
+  ///////////////////////////////////////////
+  /******** State varaibles assignment ******/
 
-function App() {
+  const [show, setShow] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={() => setShow((show) => !show)}> Show / Hide</button>
+      {show ? <h1>Welcome to React Challenges</h1> : null}
     </div>
   );
 }
-
-export default App;
